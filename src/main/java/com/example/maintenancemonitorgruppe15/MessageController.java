5a0999941b12a9a865e76eb37c68a70729b7875c
Rafin Ifta Alam <wi22b015@technikum-wien.at>
2023-06-23 21:34:30 +0200

    added feature/set + test

--- src/main/java/com/example/maintenancemonitorgruppe15/MessageController.java
+++ src/main/java/com/example/maintenancemonitorgruppe15/MessageController.java
@@ -1,14 +1,24 @@
 package com.example.maintenancemonitorgruppe15;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
     String message = "Everything works as expected"; //default message
+    String currentMessage= message;
 
     @RequestMapping("/api/message")
     public String showMessage(){ //displays the chosen message
-        return message;
+        return currentMessage;
     }
+
+    @RequestMapping("/api/message/set")
+    public String setMessage(@RequestParam String customMessage){ //changes currentMessage to the desired message
+        currentMessage= customMessage;
+        return "ok";
+    }
+
+
 }
--- src/test/java/com/example/maintenancemonitorgruppe15/MessageControllerTest.java
+++ src/test/java/com/example/maintenancemonitorgruppe15/MessageControllerTest.java
@@ -8,7 +8,19 @@ class MessageControllerTest {
 
     @Test
     void showMessage() { //test1  shows the correct message gets returned
-            MessageController mCont = new MessageController();
-            assertEquals("Everything works as expected", mCont.showMessage());
+        MessageController mCont = new MessageController();
+        String testmessage = "This is a test message";
+        mCont.setMessage(testmessage);
+        assertEquals(testmessage,mCont.showMessage());
     }
+
+    @Test
+    void setMessage(){ //test2 shows that the message was correctly changed into the desired text
+        MessageController mCont = new MessageController();
+        String test = "Hope this works";
+        mCont.setMessage(test);
+        assertEquals(test, mCont.currentMessage);
+    }
+
+
 }
