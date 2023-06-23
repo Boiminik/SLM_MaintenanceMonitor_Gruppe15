d8349fd6a93ad46ba98b3aedf95bfb293358d4c1
Dominik Pierdiluca <wi22b124@technikum-wien.at>
2023-06-23 20:15:10 +0200

    added feature/show

---
+++ src/main/java/com/example/maintenancemonitorgruppe15/MessageController.java
@@ -0,0 +1,14 @@
+package com.example.maintenancemonitorgruppe15;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    String message = "Everything works as expected"; //default message
+    
+    @RequestMapping("/api/message")
+    public String showMessage(){ //displays the chosen message
+        return message;
+    }
+}
