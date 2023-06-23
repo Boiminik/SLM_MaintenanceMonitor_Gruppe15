61c65fd37608a2bf542348d982ce213b28a596c5
Dominik Pierdiluca <wi22b124@technikum-wien.at>
2023-06-23 20:06:39 +0200

    first commit

---
+++ src/main/java/com/example/maintenancemonitorgruppe15/MaintenanceMonitorGruppe15Application.java
@@ -0,0 +1,13 @@
+package com.example.maintenancemonitorgruppe15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorGruppe15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceMonitorGruppe15Application.class, args);
+	}
+
+}
